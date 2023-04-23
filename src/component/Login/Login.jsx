85bda18290c1aff1f8d06f54982c457c879ee15c
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-04-23 18:26:23 +0600

    validation added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import { Form, Button } from 'react-bootstrap';
 const Login = () => {
+    const handleLogin=(event)=> {
+        event.preventDefault();
+        const form =event.target;
+        const email = form.email.value;
+        const password =form.password.value;
+        console.log(email,password);
+    }
     return (
-        <div>
-            <h2>Login page Here</h2>
+        <div className='w-25 mx-auto'>
+            <h2>Please Login Here</h2>
+            <Form onSubmit={handleLogin}>
+                <div className='form-group mb-3'>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" name='email' value= ''/>
+                    </Form.Group>
+                </div>
+                <div className='form-group mb-3'>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" name='password' value='' />
+                    </Form.Group>
+                </div>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -5,25 +5,45 @@ import app from '../../firebase/firebase.config';
 const auth=getAuth(app);
 
 const Register = () => {
-    const [email,setEmail] = useState('');
+    const [error,setError] = useState('');
+    const [success,setSuccess] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        setSuccess('');
+        setError('');
         const email = event.target.email.value;
         const password = event.target.password.value;
-
         console.log(email,password)
+        // validate
+        if(!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least one upper case');
+            return;
+        }
+        // else if(!/(?=.*[0-9]. *[0.9])/.test(password)) {
+        //     setError("Please add at least two number");
+        //     return;
+        // }
+        else if(password.length < 6) {
+            setError('Please Set at least 6 character to your password');
+            return;
+        }
         // create user in firebase
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // Signed in 
           const loggedUser = userCredential.user;
           console.log(loggedUser);
+          setError('');
+          event.target.reset();
+          setSuccess('User has been created Successfully')
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(errorCode,errorMessage);
+          setError(errorMessage);
+         
         });
     }
 
@@ -37,12 +57,14 @@ const Register = () => {
         <div className='w-50 mx-auto border p-4'>
             <h4 className='text-primary'>Please Register</h4>
             <form onSubmit={handleSubmit} >
-                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Enter Your Email' className='p-2 mb-1 rounded' />
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Enter Your Email' className='p-2 mb-1 rounded' required />
                 <br />
-                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Enter Your Password'className='p-2 mb-1 rounded' />
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Enter Your Password'className='p-2 mb-1 rounded'required />
                 <br />
                 <input type="submit" value="Register" className='p-2 btn btn-primary' />
             </form>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
